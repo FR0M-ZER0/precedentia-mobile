9abc6cc239bbd56580327e8c2fa0aaad79d4f187
João Suzuki <joaosuzuki98@outlook.com>
2026-05-12 10:57:43 -0300

    feat(pia-87): Adiciona conexão por variável de ambiente

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,11 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DioClient {
   DioClient._();
+  static final String precedentiaApiUrl =
+    dotenv.env['PRECEDENTIA_API_URL'] ?? '';
 
   static final Dio instance = Dio(
     BaseOptions(
-      baseUrl: 'http://localhost:8087',
+      baseUrl: precedentiaApiUrl,
       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(minutes: 5),
       sendTimeout: const Duration(minutes: 2),
