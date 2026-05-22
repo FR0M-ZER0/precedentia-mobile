651285b1e45f0ba88843fbbd1ce80b04bf2be812
gabriel <biel_google@hotmail.com>
2026-05-22 10:11:40 -0300

    fix(pia-97): Ajeita espaçamento que tava causando erro no analysis-routes

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -2,10 +2,14 @@ import 'package:dio/dio.dart';
 
 class DioClient {
   DioClient._();
+  static const String _apiUrl = String.fromEnvironment(
+    'API_URL',
+    defaultValue: 'http://localhost:8087',
+  );
 
   static final Dio instance = Dio(
     BaseOptions(
-      baseUrl: 'http://localhost:8087',
+      baseUrl: _apiUrl,
       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(minutes: 5),
       sendTimeout: const Duration(minutes: 2),
